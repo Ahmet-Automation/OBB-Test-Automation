6e413d13ed97ee1bd4f5a9c21a2e02adc23d7ddb
Ahmet T <ahmet.qa.at@gmail.com>
2026-01-01 21:01:47 +0100

    feat: add singleton Driver utility class for browser management

---
+++ src/test/java/at/obb/automation/utils/Driver.java
@@ -0,0 +1,32 @@
+package at.obb.automation.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    private static WebDriver driver;
+
+    private Driver() {
+
+    }
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3))
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver !=null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
