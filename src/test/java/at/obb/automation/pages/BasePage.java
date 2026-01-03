e8f1c0140a97a26bf697cf929bd6f25c258f5947
Ahmet T <ahmet.qa.at@gmail.com>
2026-01-03 20:39:00 +0100

    git commit -m "feat: initial BasePage structure and window handle logic (WIP)

--- src/test/java/at/obb/automation/pages/BasePage.java
+++ src/test/java/at/obb/automation/pages/BasePage.java
@@ -2,11 +2,45 @@ package at.obb.automation.pages;
 
 
 import at.obb.automation.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
-    public BasePage () {
-        // // This initializes the @FindBy annotations
-        PageFactory.initElements(Driver.getDriver(), this);
+    // Initializes the WebDriver and the WebDriverWait object for the entire project.
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+
+    public BasePage (WebDriver driver) {
+       // This initializes the @FindBy annotations
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator)).click();
+    }
+
+
+    public void sendKeys(By locator, String text) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(text);
+    }
+
+    public void switchToFrame(By frameLocator) {
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameLocator));
+    }
+
+    public void waitForDomReady() {
+        wait.until(driver ->
+                ((org.openqa.selenium.JavascriptExecutor) driver)
+                        .executeScript("return document.readyState")
+                        .equals("complete")
+        );
     }
 }
